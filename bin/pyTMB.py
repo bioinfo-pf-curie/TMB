f23362c43644f097f4f75bf753b07af602053a9b
nservant <nicolas.servant@curie.fr>
2020-04-06 12:34:16 +0200

    [MODIF] Update options

--- bin/pyTMB.py
+++ bin/pyTMB.py
@@ -41,60 +41,98 @@ Step of the script:
 from cyvcf2 import VCF
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-i", "--input", help="Input file (VCF format)")
-parser.add_argument("-b", "--bed", help="Capture design (BED file)", default='')
 
-## Thresholds
-parser.add_argument("-r", "--minAR", help="Select variants with Allelic Ratio > minAR", default='')
-parser.add_argument("-m", "--minMAF", help="Select variants with MAF > minMAF", default='') 
-parser.add_argument("-c", "--minDepth", help="Select variants with depth > minDepth", default='') 
+def isCoding(v):
+    return True
 
-## Which variants to use ?
-parser.add_argument("-uc", "--useCoding", help="Include Coding variants", default='')
-parser.add_argument("-unc", "--useNonCoding", help="Include Non-coding variants", default='') 
-parser.add_argument("-us", "--useSyn", help="Include Synonymous variants", default='') 
-parser.add_argument("-uns", "--useNonSyn", help="Include Non-Synonymous variants", default='') 
-parser.add_argument("-us", "--useSomatic", help="Include Somatic variants", default='')
-parser.add_argument("-ug", "--useGermline", help="Include Germline variants", default='') 
-parser.add_argument("-uh", "--useHotspot", help="Include variants in hotspots", default='') 
+def isNonCoding(v):
+    return True
 
-## Database
-parser.add_argument("-ds", "--somaticDb", help="Database used for somatic annotation", default='') 
-parser.add_argument("-dh", "--hotspotDb", help="Database used for hotspot annotation", default='') 
+def isSynonymous(v):
+    return True
 
-## Others
-parser.add_argument("-v", "--version", help="Pipeline version", default='')
+def isNonSynonymous(v):
+    return True
+
+def isHotspot(v):
+    return True
+
+def isSomatic(v):
+    return True
+
+def isGermline(v):
+    return True
 
 
-args = parser.parse_args()
+def argsParse():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input", help="Input file (VCF format)")
+    parser.add_argument("-b", "--bed", help="Capture design (BED file)", default='')
+    
+    ## Thresholds
+    parser.add_argument("--minAR", help="Select variants with Allelic Ratio > minAR", default='')
+    parser.add_argument("--minMAF", help="Select variants with MAF > minMAF", default='') 
+    parser.add_argument("--minDepth", help="Select variants with depth > minDepth", default=5) 
+    
+    ## Which variants to use ?
+    parser.add_argument("--useCoding", help="Include Coding variants", default='')
+    parser.add_argument("--useNonCoding", help="Include Non-coding variants", default='') 
+    parser.add_argument("--useSyn", help="Include Synonymous variants", default='') 
+    parser.add_argument("--useNonSyn", help="Include Non-Synonymous variants", default='') 
+    parser.add_argument("--useSomatic", help="Include Somatic variants", default='')
+    parser.add_argument("--useGermline", help="Include Germline variants", default='') 
+    parser.add_argument("--useHotspot", help="Include variants in hotspots", default='') 
+    
+    ## Database
+    parser.add_argument("--somaticDb", help="Database used for somatic annotation", default='') 
+    parser.add_argument("--hotspotDb", help="Database used for hotspot annotation", default='') 
+    
+    ## Others
+    parser.add_argument("-v", "--version", help="Pipeline version", default='')
+    
+    args = parser.parse_args()
+    return (args)
 
 
-for variant in VCF(args.input): # or VCF('some.bcf')
-    variant.REF, variant.ALT # e.g. REF='A', ALT=['C', 'T']
 
-    variant.CHROM, variant.start, variant.end, variant.ID, \
-                variant.FILTER, variant.QUAL
+if __name__ == "__main__":
 
-    # numpy arrays of specific things we pull from the sample fields.
-    # gt_types is array of 0,1,2,3==HOM_REF, HET, UNKNOWN, HOM_ALT
-    variant.gt_types, variant.gt_ref_depths, variant.gt_alt_depths # numpy arrays
-    variant.gt_phases, variant.gt_quals, variant.gt_bases # numpy array
+    args = argsParse()
+
+    for variant in VCF(args.input): # or VCF('some.bcf')
+
+        if variant.INFO.get('AF') > args.minMAF:
+            continue
+
+        if variant.INFO.get('DP') > args.minDepth:
+            continue
 
 
-    ## INFO Field.
-    ## extract from the info field by it's name:
-    variant.INFO.get('DP') # int
-    variant.INFO.get('FS') # float
-    variant.INFO.get('AC') # float
+        ### Usage examples of cyvcf
+        variant.REF, variant.ALT # e.g. REF='A', ALT=['C', 'T']
 
-    # convert back to a string.
-    str(variant)
-
-    ## per-sample info...
-    # Get a numpy array of the depth per sample:
-    dp = variant.format('DP')
-    # or of any other format field:
-    sb = variant.format('SB')
-    assert sb.shape == (n_samples, 4) # 4-values per
-
+        variant.CHROM, variant.start, variant.end, variant.ID, \
+            variant.FILTER, variant.QUAL
+        
+        # numpy arrays of specific things we pull from the sample fields.
+        # gt_types is array of 0,1,2,3==HOM_REF, HET, UNKNOWN, HOM_ALT
+        variant.gt_types, variant.gt_ref_depths, variant.gt_alt_depths # numpy arrays
+        variant.gt_phases, variant.gt_quals, variant.gt_bases # numpy array
+    
+    
+        ## INFO Field.
+        ## extract from the info field by it's name:
+        variant.INFO.get('DP') # int
+        variant.INFO.get('FS') # float
+        variant.INFO.get('AC') # float
+        
+        # convert back to a string.
+        str(variant)
+        
+        ## per-sample info...
+        # Get a numpy array of the depth per sample:
+        dp = variant.format('DP')
+        # or of any other format field:
+        sb = variant.format('SB')
+        assert sb.shape == (n_samples, 4) # 4-values per
+        
