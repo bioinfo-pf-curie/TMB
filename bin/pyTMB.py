6dbc68c087a232f5c13ca7dbfd6071ce85a20048
nservant <nicolas.servant@curie.fr>
2020-04-08 12:32:28 +0200

    [MODIF] First complete version

--- bin/pyTMB.py
+++ bin/pyTMB.py
@@ -57,6 +57,33 @@ def loadConfig(infile):
             raise
 
 """
+Calculate Effective Genome Size from a BED file
+"""
+def getEffGenomeSizeFromBed(infile, verbose=False):
+
+    if verbose:
+        print("## Loading BED file '", infile, "'...")
+        
+    bedhandle = open(infile)
+    effgs = 0
+    nline = 0
+    for line in bedhandle:
+        bedtab = line.strip().split("\t")
+        nline += 1
+        try:
+            chromosome, start, end = bedtab[:3]
+        except ValueError:
+            sys.stderr.write("Error : wrong input format in line", nline,". Not a BED file !?")
+            sys.exit(-1)
+
+        intl = abs(int(end) - int(start))
+        effgs += intl
+    
+    bed_handle.close()
+    return effgs
+
+
+"""
 Check if a variant has the provided annotation flags
 """
 def isAnnotatedAs(v, infos, flags):
@@ -76,15 +103,41 @@ def isAnnotatedAs(v, infos, flags):
 
     return(False)
 
+
+"""
+Check if a variant is in a genomeDb with a MAF > val
+"""
+def isGenomeDb(v, infos, flags, val):
+
+    subINFO = subsetINFO(infos, keys=flags)
+    for key in subINFO:
+        if subINFO[key] is not None and subINFO[key] != ".":
+            if float(subINFO[key]) > float(val):
+                return True
+    return(False)
+
+"""                                                                                                                                                                                                         
+Check if a variant is annotated as a cancer hotspot
+"""
+def isCancerHotspot(v, infos, flags):
+    subINFO = subsetINFO(infos, keys=flags)
+    for key in subINFO:
+        if subINFO[key] is not None and subINFO[key] != ".":
+            return True
+    return(False)
+
 """
 Subset the annotation information to a few key values
 """
 def subsetINFO(annot, keys):
 
-    subsetInfo=[]
-    for i in range(0, len(annotInfo)):
-        z = dict((k, annotInfo[i][k]) for k in keys)
-        subsetInfo.append(z)
+    if isinstance(annot, list):
+        subsetInfo=[]
+        for i in range(0, len(annot)):
+            z = dict((k, annot[i][k]) for k in keys if k in annot)
+            subsetInfo.append(z)
+    else:
+        subsetInfo = dict((k, annot[k]) for k in keys if k in annot)
 
     return(subsetInfo)
 
@@ -110,6 +163,19 @@ def annovar2dl(INFO):
     if INFO is not None:
         return [dict(INFO)]
 
+"""
+Get a tag value from either the format field or the info field
+"""
+def getTag(v, tag):
+
+    ## First check in FORMAT field
+    if tag in variant.FORMAT:
+        val=variant.format(tag, str)
+    ## Otherwise, check in INFO field
+    if tag not in variant.FORMAT or val is None:
+        val=variant.INFO.get(tag)
+
+    return float(val)
 
 """
 Parse inputs
@@ -119,7 +185,10 @@ def argsParse():
     parser.add_argument("-i", "--vcf", help="Input file (.vcf, .vcf.gz, .bcf)")
     parser.add_argument("-c", "--config", help="Databases config file", default="./config/databases.yml")
     parser.add_argument("-a", "--annot", help="Annotation format used in the vcf file", default="snpeff")
-    parser.add_argument("--bed", help="Capture design (BED file)", default=None)
+
+    ## Efective genome size
+    parser.add_argument("--effGenomeSize", help="Effective genome size", type=int, default=None)
+    parser.add_argument("--bed", help="Capture design to use if effGenomeSize is not defined (BED file)", default=None)
     
     ## Thresholds
     parser.add_argument("--minVAF", help="Filter variants with Allelic Ratio < minAR", type=float, default=0.05)
@@ -127,16 +196,17 @@ def argsParse():
     parser.add_argument("--minDepth", help="Filter variants with depth < minDepth", type=int, default=5)
     
     ## Which variants to use
+    parser.add_argument("--filterLowQual", help="Filter low quality (i.e not PASS) variant", action="store_true")
     parser.add_argument("--filterCoding", help="Filter Coding variants", action="store_true")
     parser.add_argument("--filterNonCoding", help="Filter Non-coding variants", action="store_true")
     parser.add_argument("--filterSyn", help="Filter Synonymous variants", action="store_true")
     parser.add_argument("--filterNonSyn", help="Filter Non-Synonymous variants", action="store_true")
-    parser.add_argument("--filterHotspot", help="Filter variants in hotspots", action="store_true")
-    parser.add_argument("--filterGermline", help="Filter potential variants flagged as germline in databases", action="store_true")
+    parser.add_argument("--filterCancerHotspot", help="Filter variants annotated as cancer hotspots", action="store_true")
+    parser.add_argument("--filterGenomeDb", help="Filter variants detected in large genome databases. See --minMAF", action="store_true")
     
-    ## Database
-    parser.add_argument("--germlineDb", help="Databases used for germline annotation", default="gnomAD")
-    parser.add_argument("--hotspotDb", help="Databases used for hotspot annotation", default="cosmic")
+    ## Databases
+    parser.add_argument("--genomeDb", help="Databases used for large genome annotation (comma separated)", default="gnomad")
+    parser.add_argument("--cancerDb", help="Databases used for cancer hotspot annotation", default="cosmic")
     
     ## Others
     parser.add_argument("-v", "--verbose", help="Active verbose mode", action="store_true")
@@ -154,36 +224,55 @@ if __name__ == "__main__":
 
     varCounter = 0
     varTMB = 0
+
+    if args.effGenomeSize is None:
+        if args.bed is not None:
+            effGS = getEffGenomeSizeFromBed(args.bed)
+        else:
+            sys.stderr.write("Error: Effective Genome Size not specified. See --effGenomeSize or --bed")
+            sys.exit(-1)
+    else:
+        effGS = args.effGenomeSize
     
+
     for variant in VCF(args.vcf):
         varCounter+=1
-        if (varCounter % 100 == 0):
+        if (varCounter % 1000 == 0):
             print ("## ",varCounter)
-            if varCounter == 10000:
+            if varCounter == 100000:
                 sys.exit()
 
         try:
-            ## Get annotInfo as a list of dict
+            ## All vcf INFO
+            dbInfo=dict(variant.INFO)
+
+            ## Get annotation INFO as a list of dict
             if args.annot == "snpeff":
-                annotInfo = snpEff2dl(variant.INFO.get(dbflags['tag']))
+                annotInfo = snpEff2dl(variant.INFO.get(dbflags['annotTag']))
             elif args.annot == "annovar" :
                 annotInfo = annovar2dl(variant.INFO)
 
             ## No INFO field
-            if annotInfo is None:
+            if dbInfo is None or annotInfo is None:
                 continue
-
+            
             if args.verbose:
-                print (variant.CHROM, variant.start, variant.end, annotInfo)
+                print (variant.CHROM, variant.start, variant.end, variant.QUAL, variant.FILTER, dbInfo, variant.FORMAT)
 
             ## Variant Allele Frequency
-            #if variant.INFO.get('AF') < args.minVAF:
-            #    continue
+            if getTag(variant, dbflags['freqTag']) < args.minVAF:
+                print("FILTER FREQ")
+                continue
                 
             ## Sequencing Depth
-            #if variant.INFO.get('DP') < args.minDepth:
-            #    continue
+            if getTag(variant, dbflags['depthTag']) < args.minDepth:
+                print("FILTER DEPTH")
+                continue
 
+            ## Variant has a QUAL value or not PASS in the FILTER column
+            if args.filterLowQual and (variant.QUAL is not None or variant.FILTER is not None):
+                continue
+ 
             ## Coding variants
             if args.filterCoding and isAnnotatedAs(variant, infos=annotInfo, flags=dbflags['isCoding']):
                 print("FILTER IS_CODING")
@@ -205,46 +294,37 @@ if __name__ == "__main__":
                 continue
 
             ## Hotpost
-            #if args.filterHotspot and isHotspot(v, infos=annotInfo, flags=dbflags['isHotspot'][args.hotspotDb]):
-            #    continue
+            if args.filterCancerHotspot:
+                ## Flatten list of fields
+                fdb=[]
+                for db in args.cancerDb.split(','):
+                    for x in dbflags['cancerDb'][db]:
+                        fdb.append(x)
+ 
+                if isCancerHotspot(variant, infos=dbInfo, flags=fdb):
+                    print("FILTER HOSTPOT")
+                    continue
 
-            ## Germline
-            #if args.filterGermline and isGermline(v, infos=annotInfo, flags=dbflags['isGermline'][args.germlineDb]):
-            #    continue
+            ## Large Genome Cohort
+            if args.filterGenomeDb :
+                ## Flatten list of fields
+                fdb=[]
+                for db in args.genomeDb.split(','):
+                    for x in dbflags['genomeDb'][db]:
+                        fdb.append(x)
 
+                if isGenomeDb(variant, infos=dbInfo, flags=fdb, val=args.minMAF):
+                    print("FILTER MAF")
+                    continue
+
+                ## Still alive
+                varTMB += 1
         except:
             ## TODO - try to catch different cases or at least to print info to check what's going on and where
             warnings.warn("Warning ...")
             raise
 
-        ### Usage examples of cyvcf
-        #variant.REF, variant.ALT # e.g. REF='A', ALT=['C', 'T']
-
-        #variant.CHROM, variant.start, variant.end, variant.ID, \
-        #    variant.FILTER, variant.QUAL
-        
-        # numpy arrays of specific things we pull from the sample fields.
-        # gt_types is array of 0,1,2,3==HOM_REF, HET, UNKNOWN, HOM_ALT
-        #variant.gt_types, variant.gt_ref_depths, variant.gt_alt_depths # numpy arrays
-        #variant.gt_phases, variant.gt_quals, variant.gt_bases # numpy array
-    
-    
-        ## INFO Field.
-        ## extract from the info field by it's name:
-        #variant.INFO.get('DP') # int
-        #variant.INFO.get('FS') # float
-        #variant.INFO.get('AC') # float
-        
-        # convert back to a string.
-        #str(variant)
-        
-        ## per-sample info...
-        # Get a numpy array of the depth per sample:
-        #dp = variant.format('DP')
-        # or of any other format field:
-        #sb = variant.format('SB')
-        #assert sb.shape == (n_samples, 4) # 4-values per
-
-        ## Still alivie = use this variant for TMB calculation
-        varTMB += 1
+        ## Calculate TMB
+        TMB = float(varTMB)/float(effGS)*1e6
+        print("TMB=",TMB)
 
