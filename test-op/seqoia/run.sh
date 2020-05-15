7121aa0e76eee642d074088aed1a946107f5a797
Gutman Tom <tom.gutman@.curie.fr>
2020-05-15 23:42:33 +0200

    test unitaire seqoia modifié

--- test-op/seqoia/run.sh
+++ test-op/seqoia/run.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-time python /data/users/nservant/GitLab/tmb/bin/pyTMB.py \
--i /data/tmp/tgutman/SeqOIA/TMB/MAP586_cancer_04022020_wes_only.vcf \
---bed /data/tmp/tgutman/SeqOIA/TMB/Merge_CORE-SPIKE_GRCh38.merged.bed \
---dbConfig /data/users/nservant/GitLab/tmb/config/snpeff.yml \
---varConfig /data/users/nservant/GitLab/tmb/config/mutect2.yml \
---filterNonCoding \
---filterSplice \
---filterSyn \
---filterPolym --polymDb 1k,gnomad \
---filterLowQual \
---export
+echo " "
+echo "Script to ensure pyTMB.py is working with SeqOIA WES vcfs."
+echo " "
 
+SCRIPT_DIR=/bioinfo/users/tgutman/Documents/Tom/Pipelines/tmb;
+INPUT_DIR=/data/tmp/tgutman/SeqOIA/TMB
+
+time python ../../bin/pyTMB.py \
+    -i $INPUT_DIR/MAP586_cancer_curie_final_no_reheader.vcf \
+    --bed $INPUT_DIR/Merge_CORE-SPIKE_GRCh38.merged.bed \
+    --vaf 0.05 --maf 0.001 --minDepth 20 --minAltDepth 3 \
+    --filterLowQual --filterNonCoding --filterPolym --filterSyn \
+    --polymDb 1k,gnomad \
+    --dbConfig ../../config/snpeff.yml \
+    --varConfig ../../config/mutect2.yml
