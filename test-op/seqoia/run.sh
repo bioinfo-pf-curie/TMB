6e8771c0e3ef51e1e6bb586f4b2525fffcf45bc2
Gutman Tom <tom.gutman@.curie.fr>
2020-05-19 00:37:57 +0200

    add dataset test-op seqoia

--- test-op/seqoia/run.sh
+++ test-op/seqoia/run.sh
@@ -4,12 +4,10 @@ echo " "
 echo "Script to ensure pyTMB.py is working with SeqOIA WES vcfs."
 echo " "
 
-SCRIPT_DIR=/bioinfo/users/tgutman/Documents/Tom/Pipelines/tmb;
-INPUT_DIR=/data/tmp/tgutman/SeqOIA/TMB
 
 time python ../../bin/pyTMB.py \
-    -i $INPUT_DIR/MAP586_cancer_curie_final_no_reheader.vcf \
-    --bed $INPUT_DIR/Merge_CORE-SPIKE_GRCh38.merged.bed \
+    -i head_vcf_seqOIA.vcf.gz \
+    --effGenomeSize 33958083 \
     --vaf 0.05 --maf 0.001 --minDepth 20 --minAltDepth 3 \
     --filterLowQual --filterNonCoding --filterPolym --filterSyn \
     --polymDb 1k,gnomad \
